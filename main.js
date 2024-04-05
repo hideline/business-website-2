78aab8cc96e70273a483b78315e6924c54150808
hideline <emysgalippa@gmail.com>
2024-04-05 16:21:57 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+
+function darkMode() {
+  document.querySelector('#test').classList.toggle('dark')
+}
+
+function logtest() {
+  document.querySelector('#test').classList.toggle('dark')
+  darkMode()
+}
+
+
+
+
+
+
+
+// colors: {
+//   primary: primaryColor,
+//   secondary: secondaryColor,
+// },
+
+
+
+
+
+
+// const primaryColor = {
+//   light: '#FF0000', // Default primary color light
+//   dark: '#0000FF',  // Default primary color dark
+// };
+
+// const secondaryColor = {
+//   light: '#6C757D', // Default secondary color light
+//   dark: '#CCCCCC',  // Default secondary color dark
+// };
+
+// if (process.env.NODE_ENV === 'production' && process.env.THEME === 'dark') {
+//   primaryColor = '#0000FF'; // Blue for dark theme
+//   secondaryColor = '#CCCCCC'; // Darker gray for dark theme
+// }
